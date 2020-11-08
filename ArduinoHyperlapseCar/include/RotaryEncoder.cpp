0b886604103472dbccb42d62c47c0046b2ba4a84
Jakob Richter <code@jakob-r.de>
2020-11-08 14:28:40 +0100

    arduinohyperlapsecar

---
+++ ArduinoHyperlapseCar/include/RotaryEncoder.cpp
@@ -0,0 +1,19 @@
+// Rotary Encoder
+
+class RotaryEncoder {
+  bool state_both_on = false;
+  bool state_upwards = false;
+  bool state_downwards = false;
+  bool state_button = false;
+  
+  
+  public:
+    unsigned short int button_count = 0;
+    unsigned short int rotary_position = 0;
+
+    void setup() {
+
+    }
+
+  
+}
