4cff40a888d09ef74ce4ea694ec2f8b860bdab11
Jakob Richter <code@jakob-r.de>
2020-11-08 14:58:36 +0100

    in class

---
+++ ArduinoHyperlapseCar/include/RotaryEncoder.h
@@ -0,0 +1,65 @@
+// Rotary Encoder
+
+class RotaryEncoder {
+  bool state_both_on = false;
+  bool state_upwards = false;
+  bool state_downwards = false;
+  bool state_button = false;
+
+  // Rotary Encoder
+  short int m_pin_A; // cll pin
+  short int m_pin_B; // dt pin
+  short int m_pin_button; // sw pin
+  
+  
+  public:
+    unsigned short int button_count = 0;
+    short int rotary_position = 0;
+
+    RotaryEncoder(short int pin_A, short int pin_B, short int pin_button) {
+      m_pin_A = pin_A;
+      m_pin_B = pin_B;
+      m_pin_button = pin_button;
+    }
+    
+
+    void setup() {
+      pinMode(m_pin_A, INPUT);
+      pinMode(m_pin_B, INPUT);
+      pinMode(m_pin_button, INPUT_PULLUP);
+    }
+
+    void encoderISR() {
+      bool pin_A_val = digitalRead(m_pin_A);
+      bool pin_B_val = digitalRead(m_pin_B);
+
+      if (pin_A_val == pin_B_val) { // steady position
+        state_both_on = pin_A_val;
+        state_upwards = state_downwards = false;
+      }
+      if (!state_both_on) { // if both pins were off
+        if (pin_A_val == true && pin_B_val == false && !state_upwards) {
+          rotary_position++;
+          state_upwards = true;
+          state_downwards = false;
+        }
+        if (pin_A_val == false && pin_B_val == true && !state_downwards) {
+          rotary_position--;
+          state_upwards = false;
+          state_downwards = true;
+        }
+      } else { // if poth pins were on
+        if (pin_A_val == false && pin_B_val == true && !state_upwards) {
+          rotary_position++;
+          state_upwards = true;
+          state_downwards = false;
+        }
+        if (pin_A_val == true && pin_B_val == false && !state_downwards) {
+          rotary_position--;
+          state_upwards = false;
+          state_downwards = true;
+        }
+      }
+    }
+  
+};
--- ArduinoHyperlapseCar/src/main.cpp
+++ ArduinoHyperlapseCar/src/main.cpp
@@ -3,6 +3,8 @@
 #include <TimerOne.h>
 #include <Wire.h>
 
+#include "RotaryEncoder.h"
+
 // PINS
 
 // LCD
@@ -11,86 +13,46 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 
 // Rotary Encoder
-const int pin_A = 0;   //ky-040 clk pin, interrupt & add 100nF/0.1uF capacitors between pin & ground!!!
-const int pin_B = 1;    //ky-040 dt  pin,             add 100nF/0.1uF capacitors between pin & ground!!!
-const int pin_button = 12;    //ky-040 sw  pin, interrupt & add 100nF/0.1uF capacitors between pin & ground!!!
+const short int pin_A = 0;   //ky-040 clk pin, interrupt & add 100nF/0.1uF capacitors between pin & ground!!!
+const short int pin_B = 1;    //ky-040 dt  pin,             add 100nF/0.1uF capacitors between pin & ground!!!
+const short int pin_button = 12;    //ky-040 sw  pin, interrupt & add 100nF/0.1uF capacitors between pin & ground!!!
+RotaryEncoder rot_enc(pin_A, pin_B, pin_button);
 
-// variables
-uint16_t button_count = 0;
-int16_t rotary_position = 0;
-
-bool state_both_on = false;
-bool state_upwards = false;
-bool state_downwards = false;
-bool state_button = false;
-void encoderISR() {
-  // encoder.readAB();
-  bool pin_A_val = digitalRead(pin_A);
-  bool pin_B_val = digitalRead(pin_B);
-
-  if (pin_A_val == pin_B_val) { // steady position
-    state_both_on = pin_A_val;
-    state_upwards = state_downwards = false;
-  }
-  if (!state_both_on) { // if both pins were off
-    if (pin_A_val == true && pin_B_val == false && !state_upwards) {
-      rotary_position++;
-      state_upwards = true;
-      state_downwards = false;
-    }
-    if (pin_A_val == false && pin_B_val == true && !state_downwards) {
-      rotary_position--;
-      state_upwards = false;
-      state_downwards = true;
-    }
-  } else { // if poth pins were on
-    if (pin_A_val == false && pin_B_val == true && !state_upwards) {
-      rotary_position++;
-      state_upwards = true;
-      state_downwards = false;
-    }
-    if (pin_A_val == true && pin_B_val == false && !state_downwards) {
-      rotary_position--;
-      state_upwards = false;
-      state_downwards = true;
-    }
-  }
+void rot_enc_interrupt() {
+  rot_enc.encoderISR();
 }
 
 void setup() {
   Timer1.initialize();
-
-  Timer1.attachInterrupt(encoderISR, 10000); 
+  Timer1.attachInterrupt(rot_enc_interrupt, 10000); 
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   // Print a message to the LCD.
   lcd.print(F("Starting..."));                                             //(F()) saves string to flash & keeps dynamic memory free
-  pinMode(pin_A, INPUT);
-  pinMode(pin_B, INPUT);
-  pinMode(pin_button, INPUT_PULLUP);
+
   delay(500);
   lcd.clear();
   lcd.print(F("POSITION:"));
   lcd.setCursor(0, 1);                                                       //set 1-st column, 2-nd row
   lcd.print(F("BUTTON  :"));
   lcd.setCursor(10, 1);
-  lcd.print(button_count);
+  lcd.print(rot_enc.button_count);
 }
 
 uint16_t display_button_count = 0;
 int16_t display_rotary_position = 0;
 
 void loop() {
-  if (rotary_position != display_rotary_position) {
+  if (rot_enc.rotary_position != display_rotary_position) {
     lcd.setCursor(10, 0);
     lcd.print(F("    "));
     lcd.setCursor(10, 0);
-    lcd.print(rotary_position);
-    display_rotary_position = rotary_position;
+    lcd.print(rot_enc.rotary_position);
+    display_rotary_position = rot_enc.rotary_position;
   }
   
-  button_count = digitalRead(pin_button);
-  if (display_button_count != button_count) {
+  int button_count = digitalRead(pin_button);
+  if (display_button_count != rot_enc.button_count) {
     lcd.setCursor(10, 1);
     lcd.print(button_count);
     display_button_count = button_count;
